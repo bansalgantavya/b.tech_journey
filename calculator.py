54a7ff41e5e82d72986c3b42c5c27cde98343465
bansalgantavya <147243873+bansalgantavya@users.noreply.github.com>
2024-01-31 17:10:07 +0530

    calculator.py
    a program taking two integer input as x and y and printing all the basic mathe matical operations. there may be some error in answers related to division because of restriction of answers to integer form.
    i prefer you to take the first value bigger than the second to prevent any error.

---
+++ calculator.py
@@ -0,0 +1,9 @@
+x= int(input())
+y= int(input())
+print(x+y)
+print(x-y)
+print(x*y)
+print(x**y) #power of function
+print(x/y) #provied ans in float
+print(x//y) #provide ans in int form
+print(x%y) #provides remainder
